690507c7540bbbf6afd4cc039a65955a1c4ab3d1
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-10 14:11:27 +0100

    Add routes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 
 import reducers from './reducers';
-
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
