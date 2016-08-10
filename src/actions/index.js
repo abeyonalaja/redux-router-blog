b14e9b106cab415e10d60c028c8fad6855bcfd01
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-10 16:39:07 +0100

    Add post component, and post reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=jhasvjkatysdi76atsgdjhbaksjydtuktsadifgaskjdbfjkahsdtfi87a';
 
 export function fetchPosts() {
-  const request axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type    : FETCH_POSTS,
---
+++ src/reducers/posts.js
@@ -0,0 +1,13 @@
+
+import { FETCH_POSTS } from '../actions/index';
+
+const INITAIL_STATE = { all: [], post : null };
+
+export default function(state = INITAIL_STATE, action) {
+  switch(action.type) {
+    case FETCH_POSTS:
+      return { ...state, all : action.payload.data }
+    default:
+    return state;
+  }
+}
